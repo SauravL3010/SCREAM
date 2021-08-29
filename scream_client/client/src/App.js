6bdd13ab1c74f759deb9dbbcf979df6c1d84f223
Saurav Gurhale <slgurhal@uwaterloo.ca>
2021-08-29 09:07:32 -0400

    new pages routes

--- scream_client/client/src/App.js
+++ scream_client/client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+//components
+import NavBar from './components/NavBar'
+
+// pages
+import home from './pages/home'
+import login from './pages/login'
+import signup from './pages/signup'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <Router>
+        <NavBar />
+          <div className="container">
+          <Switch>
+            <Route exact path='/' component={home} />
+            <Route exact path='/login' component={login} />
+            <Route exact path='/signup' component={signup} />
+          </Switch>
+          </div>
+      </Router>
     </div>
   );
 }
---
+++ scream_client/client/src/components/NavBar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom';
+
+// MUI stuff
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
+
+
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <AppBar position="fixed">
+                    <Toolbar className='nav-container'>
+                        <Button color='inherit' component={Link} to='/login'>LOGIN</Button>
+                        <Button color='inherit' component={Link} to='/'>HOME</Button>
+                        <Button color='inherit' component={Link} to='/signup'>SIGNUP</Button>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
+    }
+}
+
+export default NavBar
